fc87e6014685fa6ab4fbca4df45f16443c67d0ff
Banana <lh1227@yahoo.co.kr>
2021-08-03 00:34:16 +0900

    필수 네트워크 스크립트 생성

---
+++ Network/PacketType.py
@@ -0,0 +1,26 @@
+from enum import Enum
+
+
+class MessageType(Enum):
+    CONNECT = 0x00
+    DISCONNECT = 0x01
+
+    INFORMATION_REQUEST = 0x02
+    CONTROL = 0x03
+
+    CONNECT_CHECK = 0x04
+
+    ALL_STOP = 0x05
+
+    DEFAULT = 0xFF
+
+
+class ObjectType(Enum):
+    JETBOT = 0x01
+    XAVIER = 0x02
+    TEST = 0x01
+
+
+class MoveState:
+    GO = 0x00
+    STOP = 0x01
---
+++ Network/UDPSocket.py
@@ -0,0 +1,28 @@
+import socket
+
+from Network.SocketAddress import SocketAddress
+from Network.Packet import *
+
+
+class UDPSocket:
+    def __init__(self, address: SocketAddress, bufferSize: int = 2048) -> None:
+        self.__bufferSize = bufferSize
+
+        self.__socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.__socket.setblocking(False)
+
+    def onDestory(self) -> None:
+        self.__socket.close()
+
+    def sendTo(self, outputPacket: OutputPacket, address: SocketAddress) -> None:
+        self.__socket.sendto(outputPacket.data, address.address)
+
+    def receiveFrom(self) -> tuple or None:
+        try:
+            data, address = self.__socket.recvfrom(self.__bufferSize)
+            if len(data) >= 0:
+                return data, address
+        except ConnectionResetError:
+            return -1
+        except BlockingIOError:
+            return None
